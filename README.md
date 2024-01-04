06bb87adbfaafc172090d3e89237fcf8d3cedfa6
JaceLau <1446103183@qq.com>
2024-01-04 20:08:39 +0800

    update

--- README.md
+++ README.md
@@ -1,48 +1,27 @@
-# Kubernetes Cluster using Virtual Box
 
-This is a quick and basic setup for a single-master, duo worker node `Kubernetes` cluster using VirtualBox to virtualize the nodes. The setup uses vagrant to quickly pull VM images (`Ubuntu/Focal64`) and bootstraps the nodes, installing `docker`, `kubelet`, `kubectl`, `kubeadm` and the `Calico CNI`. This is tested for K8s `1.24.0` and kubectl `1.24.0`.
+# 使用vagrant快速搭建某个版本的k8s
+1. 安装virtualbox与vagrant
+2. vagrant需要基础vbox镜像，国内网络可以通过清华源下载vagrant box镜像
+    - vagrant box add ubuntu/focal64 https://mirrors.tuna.tsinghua.edu.cn/ubuntu-cloud-images/focal/current/focal-server-cloudimg-amd64-vagrant.box
+    - vagrant box add ubuntu/jammy64 https://mirrors.tuna.tsinghua.edu.cn/ubuntu-cloud-images/jammy/current/jammy-server-cloudimg-amd64-vagrant.box
 
-## Requirements
+## 本文采用cri-docker和docker来安装k8s
+1.24之后的k8s，需要cri-docker, 下载cri-dockerd的安装包放到对应的shared目录下
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.7/cri-dockerd_0.3.7.3-0.ubuntu-jammy_amd64.deb
 
-- Windows (`amd64`) Host Machine
-- `Vagrant`(>=`2.2.19`): To quickly provision and bootstrap the nodes
-- `VirtualBox`(>=`6.1.30`): For virtualization of the nodes
-- `Kubernetes` (>=`1.24.0`): Tested on this version
-- \>= 2 CPUs and >= 2 GB RAM per node (configurable in `Vagrantfile`)
-- Each node must have a unique **private IP** and **hostname** on the `VirtualBox` host network. (configurable in `Vagrantfile` under the key `config.vm.network` and `config.vm.provision` respectively)
-- The K8s cluster uses the [Calico](https://projectcalico.docs.tigera.io/getting-started/kubernetes/self-managed-onprem/onpremises#install-calico) as a node networking solution.
+## 配置
+修改相关文件内的设备配置，如ip，设备名等(用 ###注释###的地方)
 
-## Usage
+## 启动
+进入kube-master/kube-w1/kube-w2目录，执行`vagrant up`
 
-1. Ensure that the `VirtualBox` UI is running.
-2. Go to `./kube-master` and run `vagrant up`
-3. Ensure that the `kube-master` node is properly setup by running `kubectl get nodes` and `kubectl get pods --all-namespaces`, all pods should show `1/1` and the master node should be `READY`.
-4. Retrieve the following command using `sudo cat /kubeadm.log` in the `kube-master`'s directory. Ensure that `sudo` is present when running it on the worker node VMs (steps 5,7).
+## 说明
+本仓库脚本使用cri-docker和docker方案，所以kubeadm命令需要手动指定 --cri-socket /run/cri-dockerd.sock
 
-```bash
-sudo kubeadm join <NODE_IP>:6443 --token <TOKEN> \
-        --discovery-token-ca-cert-hash sha256:<HASH>
-```
+- sudo kubeadm reset --cri-socket /run/cri-dockerd.sock
+- kubeadm init --kubernetes-version=v1.28.0 --pod-network-cidr=10.244.0.0/16 --cri-socket /run/cri-dockerd.sock
+- kubeadm join --cri-socket /run/cri-dockerd.sock ...
 
-5. Go to `./kube-w1` and run `vagrant up`
-6. Run the command from step `4`.
-7. Go to `./kube-w2` and run `vagrant up`
-8. Run the command from step `4`.
-9. Go back to `kube-master` and run `kubectl get nodes`, all 3 nodes should show as `READY`
 
-## Default credentials
-
-The vagrant default credentials for the vm are `vagrant:vagrant`. This can be configured in the `Vagrantfile`. Refer to the Vagrant documentation.
-
-## To use custom SSH Key for the VMs (Optional)
-
-The script will look out for `./id_rsa.pub` in the root directory. If present, the SSH key will be uploaded to all of the nodes.
-
-> The `id_rsa.pub` is git ignored and will need to be defined after cloning.
-
-## Bootstrap Script Args
-
-The kube installation script runs when the VM is **provisioned** and first started up and is called `bootstrap-kube-xx.sh`. The bash script takes in 2 positional arguments.
-
-1. The hostname to set for the node
-1. The kubernetes version to install ( `1.24.0` | `x.x.x` )
+### refs:
+https://www.jjworld.fr/kubernetes-installation/
--- kube-w1/bootstrap-kube-worker.sh
+++ kube-w1/bootstrap-kube-worker.sh
@@ -1,50 +1,113 @@
 #!/bin/bash
 whoami
-HOSTNAME=$1
-KUBE_VER=$2
+KUBE_VER=$1
+HOSTNAME=$2
+sudo hostnamectl set-hostname $HOSTNAME
 HOST_PRIVATE_IP=$(hostname -I | cut -d ' ' -f2)
 printf "Installing K8=$KUBE_VER-00 on $HOSTNAME with IP: $HOST_PRIVATE_IP\n"
 
-if [ -f "/home/vagrant/.ssh/id_rsa.pub" ]
-then
-  cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-  printf "Added id_rsa.pub to server!\n"
-fi
+# set dns
+echo "nameserver 114.114.114.114" | sudo tee /etc/resolv.conf > /dev/null
 
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install ca-certificates software-properties-common apt-transport-https curl gnupg lsb-release -y
 
-sudo hostnamectl set-hostname $HOSTNAME
-sudo swapoff -a && sudo sed -i 's/\/swap/#\/swap/g' /etc/fstab
+# add host
+### 修改每个机器对应的host ###
+echo "192.168.33.10 kube-master" | sudo tee -a /etc/hosts
+echo "192.168.33.11 kube-w1" | sudo tee -a /etc/hosts
+echo "192.168.33.12 kube-w2" | sudo tee -a /etc/hosts
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list >/dev/null
 
-sudo apt-get update
-sudo apt-get install -y docker.io
-sudo apt-mark hold docker.io
+# install
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get -y install net-tools iputils-ping ca-certificates software-properties-common apt-transport-https curl gnupg lsb-release
+ifconfig
 
+
+# install docker : https://docs.docker.com/engine/install/ubuntu/
+# Add Docker's official GPG key:
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Docker version 24.0.7, build afdd53b
+
+# without this 
 sudo tee /etc/docker/daemon.json >/dev/null <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
+  "registry-mirrors": ["https://h89hplon.mirror.aliyuncs.com"]
 }
 EOF
 
-sudo mkdir -p /etc/systemd/system/docker.service.d
-sudo systemctl daemon-reload
 sudo systemctl restart docker
+sudo systemctl enable docker
 
-sudo apt-get install -y kubelet=$KUBE_VER-00 kubeadm=$KUBE_VER-00 kubectl=$KUBE_VER-00
+
+# close firewall
+# ubuntu 22 default status of ufw is inactive, so we don't need
+# sudo ufw disable
+
+# close selinux
+# ubuntu 22 didn't installed selinux, so we don't need close it
+
+# close swap
+sudo swapoff -a && sudo sed -i 's/\/swap/#\/swap/g' /etc/fstab
+
+
+# install kubenetes
+curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://mirrors.aliyun.com/kubernetes/apt kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
+
+sudo apt-get update && sudo apt-get install -y kubelet=$KUBE_VER-00 kubeadm=$KUBE_VER-00 kubectl=$KUBE_VER-00
 sudo apt-mark hold kubelet kubeadm kubectl
+echo "KUBELET_EXTRA_ARGS=--node-ip=$HOST_PRIVATE_IP" | sudo tee /etc/default/kubelet > /dev/null
+sudo systemctl enable kubelet
 
-echo "KUBELET_EXTRA_ARGS=--node-ip=$HOST_PRIVATE_IP" >> /etc/default/kubelet
+
+# install cri-docker from https://github.com/Mirantis/cri-dockerd/releases, 本脚本将下载好的deb包放置到了贡献目录/shared下
+sudo apt install -y /shared/cri-dockerd.deb
 sudo systemctl daemon-reload
-sudo systemctl restart kubelet
+sudo systemctl enable --now cri-docker.socket
 
-echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+
+# set
+sudo modprobe br_netfilter
+echo "net.bridge.bridge-nf-call-ip6tables = 1" | sudo tee -a /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-iptables = 1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
+echo "vm.swappiness=0" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
+
+
+# 影响kubeadm不能启动的一个关键原因 ：需要指定cri-docker的参数使用 [config/images] Pulled registry.aliyuncs.com/google_containers/pause:3.9 （此版本为拉取镜像时候对应适配的版本）
+# /lib/systemd/system/cri-docker.service  if cri-docker isn't enabled， change this file
+# /etc/systemd/system/multi-user.target.wants/cri-docker.service  if cri-docker is enabled， change this file
+# 脚本修改
+sudo kubeadm config images list --image-repository registry.aliyuncs.com/google_containers --kubernetes-version v$KUBE_VER > /shared/images.list
+for line in $(cat /shared/images.list)
+do
+  ifpause=$(echo $line | grep "pause")
+  if [[ "$ifpause" != "" ]]; then
+    pattern="ExecStart=\/usr\/bin\/cri-dockerd --container-runtime-endpoint fd:\/\/"
+    ifhasset=$(cat /etc/systemd/system/multi-user.target.wants/cri-docker.service | grep "pause")
+    if [[ "$ifhasset" == "" ]]; then
+      append_string="  --pod-infra-container-image=$line"
+      sudo sed -i "s|$pattern|$pattern$append_string|" /etc/systemd/system/multi-user.target.wants/cri-docker.service
+    fi
+  fi
+done
+# 手动修改
+# sudo vim /etc/systemd/system/multi-user.target.wants/cri-docker.service， 在ExecStart=/usr/bin/cri-dockerd --container-runtime-endpoint fd:后添加
+# --pod-infra-container-image=registry.aliyuncs.com/google_containers/pause:3.9  # 对应/shared/images.list中pause的版本
+sudo systemctl daemon-reload
+sudo systemctl restart cri-docker
+
+
+# kubeadm join
